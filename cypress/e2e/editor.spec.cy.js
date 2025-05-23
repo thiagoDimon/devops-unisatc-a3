50928da94b726f0055b5114b55c0d935f31dc04c
Thiago Dimon Miranda <othiago.miranda@outlook.com>
2025-05-22 21:23:32 -0300

    Inicia teste do editor

--- cypress/e2e/editor.spec.cy.js
+++ cypress/e2e/editor.spec.cy.js
@@ -5,6 +5,12 @@ describe('Testes do Usuário Editor', () => {
   });
 
   it('Cadastrando um autor', () => {
-    cy.get('[input]');
+    cy.get('a[aria-label="Content Manager"]').click();
+    cy.get('span').contains('Autor').click();
+    cy.get('span').contains('Create new entry').click();
+    cy.get('#:r1c:').type('Usuário Autor 1');
+    cy.get('#:r1d:').type('autor1@autor.com.br')
+    cy.get('section[aria-label="Avatar"]').click();
+    // cy.get('input[type=file]').selectFile('file.json')
   });
 });
