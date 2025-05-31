84433d5ff734703e4ea3e28fc70ac2d649c68a36
thiagoDimon <othiago.miranda@outlook.com>
2025-05-31 17:26:37 -0300

    Adiciona teste do editor

--- cypress/e2e/editor.spec.cy.js
+++ cypress/e2e/editor.spec.cy.js
@@ -1,16 +1,36 @@
 describe('Testes do Usuário Editor', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('http://localhost:1337/');
     cy.login('editor@satc.edu.br', 'welcomeToStrapi123');
   });
 
-  it('Cadastrando um autor', () => {
+  it('Cadastrando autor', () => {
     cy.get('a[aria-label="Content Manager"]').click();
-    cy.get('span').contains('Autor').click();
+    cy.get('a > div > span').contains('Autor').click();
     cy.get('span').contains('Create new entry').click();
-    cy.get('#:r1c:').type('Usuário Autor 1');
-    cy.get('#:r1d:').type('autor1@autor.com.br')
+    cy.get('input[name="name"]').type('Usuário Autor 1');
+    cy.get('input[name="email"]').type('autor1@autor.com.br')
     cy.get('section[aria-label="Avatar"]').click();
-    // cy.get('input[type=file]').selectFile('file.json')
+    cy.get('button[role="checkbox"]').click()
+    cy.get('button > span').contains('Finish').click();
+    cy.get('button > span').contains('Save').click();
+    cy.get('h1').contains('Usuário Autor 1');
   });
+
+  it('Editando autor cadastrado', () => {
+    cy.get('a[aria-label="Content Manager"]').click();
+    cy.get('a > div > span').contains('Autor').click();
+    cy.get('[aria-rowindex="3"]').click()
+    cy.get('input[name="name"]').clear().type('Usuário Autor Alterado');
+    cy.get('input[name="email"]').clear().type('autor.alterado@autor.com.br')
+    cy.get('button > span').contains('Save').click();
+  })
+
+  it('Deletando autor cadastrado', () => {
+    cy.get('a[aria-label="Content Manager"]').click();
+    cy.get('a > div > span').contains('Autor').click();
+    cy.get('td[role="gridcell"]:last > button').click();
+    cy.get('.rROPR').click();
+    cy.get('footer > button > span').contains('Confirm').click();
+  })
 });
